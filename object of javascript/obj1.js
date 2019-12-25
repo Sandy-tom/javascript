5a909b8861a7f898ac82efa14f0fa897f9c30730
Santhosh <49803785+Sandy-tom@users.noreply.github.com>
2019-12-25 16:18:28 +0530

    basic javascript object

---
+++ object of javascript/obj1.js
@@ -0,0 +1,16 @@
+
+var mj = {
+name :"sa",
+l_name : "kumar",
+number : 56576,
+age : 29,
+location : "cahennai",
+swap  :function(){
+       var i = 0;
+       i = i + this.age;
+       return i;
+}
+};
+
+console.log(mj.age);
+console.log(mj.swap());
