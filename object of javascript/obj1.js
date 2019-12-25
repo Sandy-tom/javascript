03b0ce4a362d69dd2b62c9c192a83cf6d919de59
Santhosh <49803785+Sandy-tom@users.noreply.github.com>
2019-12-25 16:21:16 +0530

    Update obj1.js

--- object of javascript/obj1.js
+++ object of javascript/obj1.js
@@ -5,12 +5,12 @@ l_name : "kumar",
 number : 56576,
 age : 29,
 location : "cahennai",
-swap  :function(){
+swap  :function(){   // we can insert the function in object file 
        var i = 0;
-       i = i + this.age;
+       i = i + this.age;   // this refer the owner of the object 
        return i;
 }
 };
 
 console.log(mj.age);
-console.log(mj.swap());
+console.log(mj.swap());// call the function in the object by the name of the object call function 
