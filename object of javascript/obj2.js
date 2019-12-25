bf4d01a2f07f7a5b4b3c212e29366afedadb3772
Santhosh <49803785+Sandy-tom@users.noreply.github.com>
2019-12-25 16:40:51 +0530

    Create obj2.js

---
+++ object of javascript/obj2.js
@@ -0,0 +1,14 @@
+
+// print object property by for loop
+var car = {
+
+ name : 'bmw',
+ country : 'german',
+ speed_of_car: 200
+
+}
+
+for(x in car){   // for loop for the object print in sequince
+  console.log (x);
+
+}
